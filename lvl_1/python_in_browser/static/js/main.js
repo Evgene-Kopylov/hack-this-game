b2313e594ceaba10911ffd6ee8be4005cdb45b86
Evgene-Kopylov <40310109+Evgene-Kopylov@users.noreply.github.com>
2022-10-07 05:14:04 +0200

    python + wasm ok
    lvl_1

---
+++ lvl_1/python_in_browser/static/js/main.js
@@ -0,0 +1,78 @@
+// find the output element
+const output = document.getElementById("output");
+// initializing the codemirror and pass configuration to support python and dracula theme
+const editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+              mode: {
+                  name: "python",
+                  version: 3,
+                  singleLineStringErrors: false,
+              },
+              theme: "dracula",
+              lineNumbers: true,
+              indentUnit: 4,
+              matchBrackets: true,
+            });
+// set the initial value of the editor
+editor.setValue(`
+from typing import Optional
+
+def move_rectangle(input_value: (float, float) = None) -> Optional[str]:
+    """
+    @param input_value: координаты квадрата
+    @return: Направление движения. Варианты: "Up", "Down", "Left", "Right"
+    """
+    return "Up"
+`);
+output.value = "Initializing...\n";
+
+// Add pyodide returned value to the output
+function addToOutput(stdout) {
+  output.value += ">>>\n" + stdout + "\n";
+}
+
+// Clean the output section
+function clearHistory() {
+  output.value = "";
+}
+
+// init Pyodide and show sys.version when it's loaded successfully
+async function main() {
+  let pyodide = await loadPyodide();
+  output.value = pyodide.runPython(`
+      import sys
+      sys.version
+  `);
+  output.value += `
+Python ${output.value.split(" ")[0]}
+
+`;
+  return pyodide;
+}
+// run the main funciton
+let pyodideReadyPromise = main();
+
+// pass the editor value to the pyodide.runPython function and show the result in the output section
+async function evaluatePython() {
+    let wasm_says = decodeURI(window.location.hash);
+    console.log(`js: ${wasm_says}`);
+    wasm_says = wasm_says.split(" ").slice(1);
+
+    let pyodide = await pyodideReadyPromise;
+  try {
+    pyodide.runPython(`
+      import io
+      sys.stdout = io.StringIO()
+      input_value = (${wasm_says})
+      `);
+    pyodide.runPython(`
+${editor.getValue()}    
+print(move_rectangle(input_value))
+    `);
+    let stdout = pyodide.runPython("sys.stdout.getvalue()");
+    addToOutput(stdout);
+    window.location.hash = `js_says: ${stdout.toString()}`;
+    // console.log(`js_says: ${js_says}`);
+  } catch (err) {
+    addToOutput(err);
+  }
+}
--- lvl_1/src/main.rs
+++ lvl_1/src/main.rs
@@ -1,5 +1,8 @@
 use macroquad::audio::{load_sound, Sound};
 use macroquad::prelude::*;
+// use quad_url::*;
+// use urlencoding::{decode};
+
 
 mod settings;
 use settings::*;
@@ -17,10 +20,13 @@ use crate::target_unit::TargetUnit;
 
 #[macroquad::main("breakout")]
 async fn main() {
+    info!("1___________++++++++++++++++++++++++++++++++++______________");
     let main_unit_texture: Texture2D = load_texture(MAIN_UNIT_TEXTURE_PATH).await.unwrap();
     let projectile_texture = load_texture(PROJECTILE_TEXTURE_PATH).await.unwrap();
+    info!("2___________++++++++++++++++++++++++++++++++++______________");
     let shoot_sound: Sound = load_sound(MAIN_UNIT_SHOOT_SOUND_ASSET).await.unwrap();
     let target_impact_sound: Sound = load_sound(TARGET_UNIT_IMPACT_SOUND).await.unwrap();
+    info!("3___________++++++++++++++++++++++++++++++++++______________");
     let spawn_position = (screen_width() * 0.5, screen_height() * 0.8);
     let target_unit_texture = load_texture(TARGET_UNIT_TEXTURE_PATH).await.unwrap();
     let target_unit_shadow_texture = load_texture(TARGET_UNIT_SHADOW_TEXTURE_PATH).await.unwrap();
