aa0a43a44c1d51b0b71a660da5daa9402aa31842
Evgene-Kopylov <40310109+Evgene-Kopylov@users.noreply.github.com>
2022-10-12 15:27:41 +0200

    clean

--- lvl_2/src/utils.rs
+++ lvl_2/src/utils.rs
@@ -9,8 +9,6 @@ pub fn get_parameter_value(arg: &str) -> String {
             let para = para.to_string();
             let para2: Vec<&str> = para.split('=').collect();
             if para2.len() == 2 && para2[0] == arg {
-                // info!("Para: {:?}", para2);
-                // info!("Para[0] == arg");
                 return para2[1].to_string();
             }
         }
